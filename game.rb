fbe5fe4ae51ef2ee7e43ed3cb635303c4232f4b4
Stephen Kline <Stephenkline09@gmail.com>
2020-07-30 12:01:40 -0500

    Cannot figure out the problem..

--- game.rb
+++ game.rb
@@ -1,55 +1,64 @@
 require_relative 'players'
 require_relative 'game_turn'
+require_relative 'treasure_trove'
 
 class Game
-    attr_reader :title
-
-    def initialize(title)
-        @title = title
-        @players = []
+  
+  attr_accessor :title
+  
+  def initialize(title)
+    @title = title
+    @players = []
+  end
+  
+  def add_players(a_player)
+    @players.push(a_player)
+  end
+  
+  def play(rounds)
+    puts "There are #{@players.size} players in #{@title}: "
+    
+    @players.each do |players|
+      puts players
     end
-
-    def add_players(a_player)
-        @players.push(a_player)
+    
+    treasures = TreasureTrove::TREASURES
+    puts "\nThere are #{treasures.size} treasures to be found:"
+    treasures.each do |treasure|
+      puts "A #{treasure.name} is worth #{treasure.points} points"
     end
-
-    def print_name_and_health(player)
-        puts "#{player.name} (#{player.health})"
+    
+    1.upto(rounds) do |round|
+      puts "\nRound #{round}:"
+      @players.each do |players|
+        GameTurn.take_turn(players)
+      end
     end
-
-    def print_stats
-        puts "\n#{@title} Statistics:"
-        strong_players, wimpy_players = @players.partition { |players| players.strong? }
-
-        puts "\n#{strong_players.size} Strong Players:"
-        strong_players.each do |players|
-            puts "#{players.name} (#{players.health})"        
-        end
-
-        puts "\n#{wimpy_players.size} Wimpy Players:"
-        wimpy_players.each do |players|
-            puts "#{players.name} (#{players.health})"
-        end
-
-        puts "\n#{@title} High Scores:"
-        @players.sort.each do |players|
-            formatted_name = players.name.ljust(20, '.')
-            puts "#{formatted_name} #{players.score}"
-        end
+  end
+    
+  def print_name_and_health(players)
+    puts "#{players.name} (#{players.health})"
+  end
+  
+  def print_stats
+    puts "\n#{@title} Statistics:"
+    
+    strong_players, wimpy_players = @players.partition { |players| players.strong? }
+    
+    puts "#{strong_players.size} strong players:"
+    strong_players.each do |players|
+      print_name_and_health(players)
+    end    
+    
+    puts "#{wimpy_players.size} wimpy players:"
+    wimpy_players.each do |players|
+      print_name_and_health(players)
+    end    
+    
+    puts "\n#{@title} High Scores:"
+    @players.sort.each do |players|
+      formatted_name = players.name.ljust(20, '.')
+      puts "#{formatted_name} #{players.score}"
     end
-
-    def play(rounds)
-        puts "There are #{@players.size} players in #{@title}:"
-        @players.each do |players|
-            puts players
-        end
-
-        1.upto(rounds) do |round|
-            puts "\nRound #{round}:"
-            @players.each do |players|
-                GameTurn.take_turn(players)
-                puts players
-            end
-        end
-    end
+  end
 end
